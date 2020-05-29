3e2383a37e30e28de3e1bf3a555e70acd2094747
unknown <w2cluster171@iiht.tech>
2020-05-29 23:42:41 +0530

    k8s

---
+++ handson2/loginservicems/src/main/java/com/ibm/handson/loginservicems/LoginservicemsApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.handson.loginservicems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginservicemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginservicemsApplication.class, args);
+	}
+
+}
---
+++ handson2/loginservicems/src/test/java/com/ibm/handson/loginservicems/LoginservicemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.handson.loginservicems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginservicemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
